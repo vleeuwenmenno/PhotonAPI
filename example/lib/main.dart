fb0a97fd97c875de62e7f1b6c696f9727a02b7c4
Menno van Leeuwen <menno@vleeuwen.me>
2019-02-08 16:45:20 +0100

    Implemented a cooldown system To prevent DDosing a server with too many requests by accident

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,10 @@ import 'package:PhotonAPI/latLng.dart';
 void main(List<String> args) async
 {
 	PhotonAPI api = new PhotonAPI("http://photon.komoot.de/api/");
-	List<Address> output = await api.resolve("Maerelaan 26");
+	List<Address> addresses = await api.resolve(query: "Ir. Lelyweg 44");
 
-	print(output);
+	for (Address a in addresses)
+	{
+		print("${a.street} ${a.housenumber}, ${a.city} ${a.postcode}"); 
+	}
 }
