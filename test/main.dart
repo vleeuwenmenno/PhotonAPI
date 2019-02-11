8e1123e87e3c72fc933be89ec0bd928228f6daa5
Menno van Leeuwen <menno@vleeuwen.me>
2019-02-11 11:19:06 +0100

    Added test file

---
+++ test/main.dart
@@ -0,0 +1,19 @@
+import 'package:PhotonAPI/photonApi.dart';
+import 'package:PhotonAPI/address.dart';
+
+void main() async
+{
+	PhotonAPI api = new PhotonAPI("http://photon.komoot.de/api/");
+	List<Address> addresses = await api.resolve(query: "Zijlweg", verbose: true);
+
+	print("Resolve multi output: ");
+	for (Address a in addresses)
+	{
+		print("${a.street} ${a.housenumber}, ${a.city} ${a.postcode}"); 
+	}
+
+	Address a = await api.resolveSingle(query: "Zijlweg", verbose: true);
+
+	print("Resolve single output: ");
+	print("${a.street} ${a.housenumber}, ${a.city} ${a.postcode}"); 
+}
